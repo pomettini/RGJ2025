85b42c65f38fc0b62be9c9ad167b50ca9e655d37
Giorgio Pomettini <giorgio.freespire@gmail.com>
2025-10-14 02:50:37 +0200

    Added utility functions to quickly cache faded and blurred images

--- source/utils.lua
+++ source/utils.lua
@@ -1,3 +1,5 @@
+local gfx <const> = playdate.graphics
+
 Utils = {}
 
 function Utils:hash32(x)
@@ -35,4 +37,28 @@ function Utils:ease(t)
     else
         return ((t - 0.4) / 0.6) ^ 4
     end
+end
+
+function Utils:cache_faded_frame(image, alpha, dither_type)
+    return image:fadedImage(alpha, dither_type)
+end
+
+function Utils:cache_faded_frames(image, frames, dither_type)
+    local temp_frames = {}
+    for i = 1, frames do
+        temp_frames[i] = self:cache_faded_frame(image, (1 / frames) * i, dither_type)
+    end
+    return temp_frames
+end
+
+function Utils:cache_blurred_frame(image, radius, num_passes, dither_type)
+    return image:blurredImage(radius, num_passes, dither_type)
+end
+
+function Utils:cache_blurred_frames(image, frames, radius, num_passes, dither_type)
+    local temp_frames = {}
+    for i = 1, frames do
+        temp_frames[i] = self:cache_blurred_frame(image, (radius / frames) * i, num_passes, dither_type)
+    end
+    return temp_frames
 end
